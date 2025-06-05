8ebec70ad08857394bbf05c858d7602baa2aa9a2
Cesar Bueno <91395027+hasbuen@users.noreply.github.com>
2025-06-05 16:27:47 -0300

    Update mecanica.js

--- mecanica.js
+++ mecanica.js
@@ -313,6 +313,10 @@ function renderizarTabela() {
         </td>
 
         <td>${reg.paliativo}</td>
+        <td>
+            <button onclick="copiarLinha(this)" title="Copiar"><i class="fas fa-copy"></i></button>
+            <button onclick="removerLinha(this)" title="Remover"><i class="fas fa-trash-alt"></i></button>
+        </td>
   `;
         tbody.appendChild(tr);
     });
