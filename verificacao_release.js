58aba120ec1c10b17d978112554f7eac4c6b3d62
Cesar Bueno <91395027+hasbuen@users.noreply.github.com>
2025-09-05 10:24:54 -0300

    Update verificacao_release.js

--- verificacao_release.js
+++ verificacao_release.js
@@ -102,7 +102,20 @@ async function carregarHistoricoLiberacoes() {
     const protocolos = await resProt.json();
 
     const tbody = document.getElementById("tabelaLiberados");
-    tbody.innerHTML = "";
+
+      // Início do loading: Insere o HTML de carregamento no corpo da tabela
+    tbody.innerHTML = `
+    <tr>
+      <td colspan="2" class="text-center py-6 text-gray-400">
+        <div class="flex items-center justify-center space-x-2">
+          <svg class="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          <span>Carregando...</span>
+        </div>
+      </td>
+    </tr>`;
 
     if (!dados || dados.length === 0) {
       tbody.innerHTML = `
